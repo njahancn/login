f8d263c291381641c079e5eb825ea62e825c2680
Nusrath Jahan <147369068+njahancn@users.noreply.github.com>
2023-10-20 12:51:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import user from './images/user.jpeg'
+import email from './images/email.jpeg'
+import password from './images/lock.jpeg'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='sub-main'>
+        <div>
+          <div className='imgs'>
+            <div className='img-container'>
+              <img src={user} alt='user' className='user'/>
+            </div>
+          </div>
+          <div>
+            <h1>Login Page</h1>
+            <div>
+              <img src={email} alt='email' className='email' />
+              <input type='text' placeholder='Enter your emial'  className='name'/>
+            </div>
+            <div className='second-input'>
+              <img src={password} alt='password' className='email' />
+              <input type='password' placeholder='Enter your password'  className='name'/>
+            </div>
+            <div className='button' > 
+            <button> Login</button>
+            </div>
+            
+              <p className='link'>
+                <a href='#'>Forgot Password</a> Or <a href='#'> Sign Up</a>
+              </p>
+          
+            
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
